c1f95a6499487b1e30137a1e83ff37638835c7b2
tempAcc-c <65510981+tempAcc-c@users.noreply.github.com>
2020-06-27 20:05:44 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,35 @@
 ## functions do
 
 ## Write a short comment describing this function
+##creates matrix that can cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
+  a <- NULL
+  set <- function(b){
+    x <<- b
+    a <<- NULL
+  }
+  get <- function() x
+  
+  setinverse <-function(solve) a <<-solve
+  getinverse <- function() a
+  list (set=set, get=get, setinverse= setinverse, getinverse= getinverse)
 
 }
 
 
 ## Write a short comment describing this function
+##checks for cache inverse. if not presetn, it calculates the inverse
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinverse()
+  if(!is.na(inv))
+  {
+    message("Getting cached inverse of matrix")
+    return (inv)
+  }
+  mat <-x$get()
+  inv <-solve(mat, ...)
+  x$setinverse()
+  inv
 }
